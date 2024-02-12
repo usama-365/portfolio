cc8b9d00059d08f495597f7fa987b7055158cc8c
Usama <usama-365@protonmail.com>
2024-02-12 11:29:46 +0500

    scaffold next app

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Usama | Personal Portfolio",
+  description:
+    "Usama, an introverted Full Stack Developer & Cyber Security expert. Dive into secure and innovative web solutions. Connect for a tech journey together!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
