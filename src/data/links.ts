7cdcaaca131d0bc6dc0410c60fb33497cd54d199
Usama <usama-365@protonmail.com>
2024-02-12 13:15:34 +0500

    add project data

---
+++ src/data/links.ts
@@ -0,0 +1,28 @@
+const LINKS = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export default LINKS;
---
+++ src/data/skills.ts
@@ -0,0 +1,23 @@
+const SKILLS = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "Framer Motion",
+] as const;
+
+export default SKILLS;
