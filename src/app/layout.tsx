306fdc0b846a4c653973639de44f2378f1d1f693
Usama <usama-365@protonmail.com>
2024-02-12 12:56:32 +0500

    add blurred circles on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} relative bg-gray-50 text-gray-950`}>
+        {/* Stylish dots */}
+        <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] sm:w-[68.75rem]" />
+        <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
+
+        {/* Actual content */}
+        {children}
+      </body>
     </html>
   );
 }
